44abfd37559b1ab3ebd3b16b800fd344c614707d
jowihall <148017202+jjoshall@users.noreply.github.com>
2024-10-08 15:44:58 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -10,15 +10,29 @@ const button = document.createElement("button");
 // Setting the text of the button
 button.textContent = "💎";
 
+// Variables to keep track of the number of clicks and seconds
 let numClicks = 0;
 let numSeconds = 0;
 const diamondsCollected = document.createElement("div");
 
-// Adding an interval to update the diamonds collected every second
+// Instead of using setInterval, we can use requestAnimationFrame to make a counter that increases based on how much time has passed
+let lastTime = 0;
+function updateCounter(time: number) {
+  if (time - lastTime >= 1000) {
+    numSeconds++;
+    diamondsCollected.textContent = `${numClicks + numSeconds} 💎`;
+    lastTime = time;
+  }
+  requestAnimationFrame(updateCounter);
+}
+requestAnimationFrame(updateCounter);
+
+/* Adding an interval to update the diamonds collected every second
 setInterval(() => {
   numSeconds++;
   diamondsCollected.textContent = `${numClicks + numSeconds} 💎`;
 }, 1000);
+*/
 
 // Adding an event listener to the button
 button.addEventListener("click", () => {
